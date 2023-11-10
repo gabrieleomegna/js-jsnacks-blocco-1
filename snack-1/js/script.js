ecf7ad81087a7dfeb35a8c3fe53a15a153ea2539
gabrieleomegna <gabrieleomegna01@gmail.com>
2023-11-10 11:58:03 +0100

    Implemented conditions and added file script in index.html

--- snack-1/js/script.js
+++ snack-1/js/script.js
@@ -3,9 +3,9 @@ const firstUserAge = parseInt (prompt('Write first user age'));
 const secondUserAge = parseInt (prompt('Write second user age'));
 
 if (firstUserAge > secondUserAge) {
-
+    console.log ('First user is older and he is:', firstUserAge)
 } else if (firstUserAge < secondUserAge) {
-
+    console.log ('Second user is older and he is:', secondUserAge)
 } else {
-    
+    console.log ('Both users have the same age:', firstUserAge )
 }
