ce1f7818e6206e0dce86921294dc0e5ab1a290ae
gabrieleomegna <gabrieleomegna01@gmail.com>
2023-11-10 11:48:51 +0100

    Setting variables

---
+++ snack-1/js/script.js
@@ -0,0 +1,3 @@
+//? chiedere all'utente con due prompt gli anni di due persone e comunicare quale delle due è più grande
+const firstUserAge = parseInt (prompt('Write first user age'));
+const secondUserAge = parseInt (prompt('Write second user age'));
