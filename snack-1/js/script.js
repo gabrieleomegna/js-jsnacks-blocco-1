1a6678460c07c55adddbf4db02bfb8bbb1a8b1d9
gabrieleomegna <gabrieleomegna01@gmail.com>
2023-11-10 11:51:06 +0100

    Setting if condition structure

--- snack-1/js/script.js
+++ snack-1/js/script.js
@@ -1,3 +1,11 @@
 //? chiedere all'utente con due prompt gli anni di due persone e comunicare quale delle due è più grande
 const firstUserAge = parseInt (prompt('Write first user age'));
 const secondUserAge = parseInt (prompt('Write second user age'));
+
+if (firstUserAge > secondUserAge) {
+
+} else if (firstUserAge < secondUserAge) {
+
+} else {
+    
+}
