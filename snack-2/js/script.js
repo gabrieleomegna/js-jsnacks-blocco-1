ea6f8173b9ab551e911e0d67454d3ae04039d034
gabrieleomegna <gabrieleomegna01@gmail.com>
2023-11-20 17:25:12 +0100

    Implemented for cycle in snack 2

--- snack-2/js/script.js
+++ snack-2/js/script.js
@@ -1,5 +1,7 @@
 // ? Scrivo in pagina i primi 6 numeri, scrivendo i numeri pari verdi. BN: numeri dispari gialli
-
-for (let i = 0; i<=6) {
-    
-}
+const wrapper = document.querySelector ('div.wrapper');
+let wrapperChild = document.createElement ('div');
+wrapper.appendChild(wrapperChild);
+for (let i = 0; i<=6; i++) {
+wrapperChild.innerHTML += `${i}`;
+};
