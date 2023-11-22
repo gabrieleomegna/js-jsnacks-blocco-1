fdce5175610cd0eb598c9f596b80044a0573a630
gabrieleomegna <gabrieleomegna01@gmail.com>
2023-11-22 18:35:13 +0100

    Implemented snack 5

--- snack-5/js/script.js
+++ snack-5/js/script.js
@@ -1,2 +1,11 @@
 // L'utente inserisce due parole in successione, con due prompt.
 // Il software stampa prima la parola più corta, poi la parola più lunga.
+
+const firstWord = prompt('Type your first word');
+const secondWord = prompt('Type your second word');
+
+if (firstWord.length > secondWord.length) {
+    console.log (`The longest word is:${firstWord}`);
+} else if (firstWord.length < secondWord.length) {
+    console.log (`The longest word is:${secondWord}`);
+}
