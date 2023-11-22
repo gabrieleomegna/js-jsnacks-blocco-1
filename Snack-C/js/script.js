808ce0b544c0c495eac44711ca5f1ad4138f224d
gabrieleomegna <gabrieleomegna01@gmail.com>
2023-11-22 23:45:44 +0100

    Implemented snack c

--- Snack-C/js/script.js
+++ Snack-C/js/script.js
@@ -1,3 +1,5 @@
 // Chiedi all'utente due numeri, uno piu' piccolo (due cifre) e uno piu' grande (almeno quattro cifre): 
 // moltiplica il primo numero per 2 finche' non arriva ad essere maggiore del secondo numero.
 
+const userFirstNumber = parseInt(prompt ('Write first number (2 digits)'));
+const userSecondNumber = parseInt(prompt ('Write second number (min 4 digits)'));
