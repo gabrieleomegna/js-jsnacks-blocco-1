d50b4a92156ce357f1b12e3a9086a3d18c99111f
gabrieleomegna <gabrieleomegna01@gmail.com>
2023-11-23 00:15:19 +0100

    Implemented snack c woth cycle for

--- Snack-C/js/script.js
+++ Snack-C/js/script.js
@@ -3,3 +3,11 @@
 
 const userFirstNumber = parseInt(prompt ('Write first number (2 digits)'));
 const userSecondNumber = parseInt(prompt ('Write second number (min 4 digits)'));
+let newSecondNumber;
+for (let i = userFirstNumber; i <= userSecondNumber; i = i * 2) {
+    if (i >= userSecondNumber / 2) {
+        newSecondNumber = i;
+    }
+    console.log (i);
+}
+console.log ('Il secondo numero finale è:', newSecondNumber*2)
